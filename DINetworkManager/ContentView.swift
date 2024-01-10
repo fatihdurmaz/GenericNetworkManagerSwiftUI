da6563c4028bd72bcdb9ad4b03e7c202ae4e2f1a
Fatih Durmaz <fatih.durmaz@outlook.com>
2024-01-11 02:33:17 +0300

    Initial Commit

---
+++ DINetworkManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DINetworkManager
+//
+//  Created by Fatih Durmaz on 11.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DINetworkManager/DINetworkManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DINetworkManagerApp.swift
+//  DINetworkManager
+//
+//  Created by Fatih Durmaz on 11.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DINetworkManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
